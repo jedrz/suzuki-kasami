f7a363fbe8c0c494cf1e6036a99968efade94822
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-12-13 21:55:07 +0100

    Add tcp server that receives a message and closes the connection

--- src/suzuki_kasami/node.clj
+++ src/suzuki_kasami/node.clj
@@ -1,5 +1,27 @@
 (ns suzuki-kasami.node
-  (:gen-class))
+   (:require
+    [manifold.deferred :as d]
+    [manifold.stream :as s]
+    [aleph.tcp :as tcp]
+    [taoensso.timbre :as log])
+   (:gen-class))
+
+(defn handle-message
+  [msg]
+  (log/info "Got message" (String. msg)))
+
+(defn handler
+  [s info]
+  (log/info "New connection" info)
+  (d/chain
+   (s/take! s)
+   (fn [msg]
+     (s/close! s)
+     (d/future (handle-message msg)))))
+
+(defn start-server
+  [options]
+  (tcp/start-server handler options))
 
 (defn -main
   [& args]
