2f04a3c7d5bd99f51a76c8a9ac6acc5f7dd05894
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-12-15 17:09:11 +0100

    Move tests requiring running server to new file

---
+++ test/suzuki_kasami/node_test_with_server.clj
@@ -0,0 +1,44 @@
+(ns suzuki-kasami.node-test-with-server
+  (:require [suzuki-kasami.node :as sut]
+            [clojure.test :refer :all]))
+
+(alter-var-root #'*load-tests* (constantly false))
+
+(def port 10001)
+
+(defn with-server
+  [f]
+  (with-open [s (sut/start-server {:port port})]
+    (f)))
+
+(use-fixtures :each with-server)
+
+(defn client
+    []
+    (sut/client :host "localhost" :port port))
+
+(deftest server
+
+  (defn no-op-handle-message [msg])
+
+  (with-redefs [sut/handle-message no-op-handle-message]
+
+    (testing "connection is opened after establishing connection"
+      (is (= (s/closed? @(client)) false)))
+
+    (testing "closing the connection after receiving a message"
+      (let [c @(client)]
+
+        @(s/put! c {:value 1})
+
+        (is (= (s/closed? c) true)))))
+
+  (testing "delivering message"
+    (let [received-msg (atom nil)]
+      (with-redefs [sut/handle-message (fn [msg] (reset! received-msg msg))]
+        (let [expected-msg {"value" 1}
+              c @(client)]
+
+          @(s/put! c expected-msg)
+
+          (is (= @received-msg expected-msg)))))))
