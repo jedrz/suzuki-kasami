8e0b5494e5c27fbac37ea92de2cbea307ff8e701
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-12-17 14:17:44 +0100

    SK message predicates

--- src/suzuki_kasami/suzuki_kasami.clj
+++ src/suzuki_kasami/suzuki_kasami.clj
@@ -6,3 +6,14 @@
    "type" "request"
    "value" {"requestNumber" request-number}})
 
+(defn request-msg?
+  [msg]
+  (= (get msg "type") "request"))
+
+(defn token-msg?
+  [msg]
+  (= (get msg "type") "token"))
+
+(defn sk-msg?
+  [msg]
+  ((some-fn request-msg? token-msg?) msg))
